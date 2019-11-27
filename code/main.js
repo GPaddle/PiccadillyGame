0ef66f1821f259a2d305e81924dd10b77d29392b
daiSKeul <martin.jossic5@etu.univ-lorraine.fr>
2019-11-27 17:45:20 +0100

    Ajout du fichier play.html sur l'URL /play

--- code/main.js
+++ code/main.js
@@ -1,13 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 const ws = require("ws");
 
-let server = http.createServer();
+let httpServer = http.createServer();
+httpServer.listen(8080);
 
-server.on("request", function(req, res) {
+httpServer.on("request", function(req, res) {
 	if(req.url == "/") {
 		res.end("Piccadilly Game");
 	} else if(req.url == "/play") {
-		res.end("Page de jeu");
+		res.setHeader("Content-Type", "text/html");
+		let file = fs.createReadStream("play.html");
+		file.pipe(res);
 	} else if(req.url == "/play/") {
 		res.writeHead(302, {"Location": "/play"});
 		res.end();
@@ -15,6 +19,4 @@ server.on("request", function(req, res) {
 		res.statusCode = 404;
 		res.end("Page inexistante");
 	}
-})
-
-server.listen(8080);
+});
