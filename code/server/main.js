e64f89676b008851879fad33d68ffe308e1eaa74
daiSKeul <martin.jossic5@etu.univ-lorraine.fr>
2019-12-02 15:31:10 +0100

    Ajout du CSS et des polices d'écriture

--- code/server/main.js
+++ code/server/main.js
@@ -2,21 +2,56 @@ const http = require("http");
 const fs = require("fs");
 const ws = require("ws");
 
-let httpServer = http.createServer();
+const httpServer = http.createServer();
 httpServer.listen(8080);
 
 httpServer.on("request", function(req, res) {
-	if(req.url == "/") {
-		res.end("Piccadilly Game");
-	} else if(req.url == "/play") {
-		res.setHeader("Content-Type", "text/html");
-		let file = fs.createReadStream("client/play.html");
-		file.pipe(res);
-	} else if(req.url == "/play/") {
-		res.writeHead(302, {"Location": "/play"});
-		res.end();
-	} else {
-		res.statusCode = 404;
-		res.end("Page inexistante");
+	switch(req.url) {
+		case "/": {
+			res.end("Piccadilly Game");
+			break;
+		}
+
+		case "/play": {
+			res.setHeader("Content-Type", "text/html");
+			let file = fs.createReadStream("client/play.html");
+			file.pipe(res);
+			break;
+		}
+
+		case "/play/": {
+			res.writeHead(302, {"Location": "/play"});
+			res.end();
+			break;
+		}
+
+		case "/style": {
+			res.setHeader("Content-Type", "text/css");
+			let file = fs.createReadStream("client/style.css");
+			file.pipe(res);
+			break;
+		}
+
+		case "/fonts/lato": {
+			res.setHeader("Content-Type", "font/ttf");
+			res.setHeader("Content-Length", 75136);
+			let file = fs.createReadStream("client/fonts/lato/lato.ttf");
+			file.pipe(res);
+			break;
+		}
+
+		case "/fonts/lato/bold": {
+			res.setHeader("Content-Type", "font/ttf");
+			res.setHeader("Content-Length", 73316);
+			let file = fs.createReadStream("client/fonts/lato/lato-bold.ttf");
+			file.pipe(res);
+			break;
+		}
+
+		default: {
+			res.statusCode = 404;
+			res.end("Page inexistante");
+			break;
+		}
 	}
 });
