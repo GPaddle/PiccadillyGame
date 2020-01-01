c8d1d51290e072504106be2b42a03ae33cb91b81
daiSKeul <martin.jossic5@etu.univ-lorraine.fr>
2020-01-01 18:42:59 +0100

    Finalisation du système de connexion

--- code/client/play.js
+++ code/client/play.js
@@ -2,17 +2,19 @@ const CLIENT_TYPE_PLAYER = 0;
 const CLIENT_TYPE_SCREEN = 1;
 
 const ADD_PLAYER = 0;
-const DEL_PLAYER = 1;
+const SET_PSEUDO = 1;
+const DEL_PLAYER = 2;
 
 const STATE_NONE = 0;
-const STATE_STATS = 1;
-const STATE_WAITING_ROOM = 2;
+const STATE_GAME_INFO = 1;
+const STATE_PLAYER_LIST = 2;
+const STATE_WAITING_ROOM = 3;
 
 function displayHome() {
 	document.body.innerHTML = `
 	<div id="player-infos">
 		<div class="player-info">Nombre de joueurs connectés : <span class="player-info-value" id="connected-players">...</span></div>
-		<div class="player-info">Nombre de joueurs minimum nécessaires : <span class="player-info-value" id="remaining-players">...</span></div>
+		<div class="player-info">Nombre de joueurs minimum nécessaires : <span class="player-info-value" id="min-players">...</span></div>
 	</div>
 	<div id="new-player-form">
 		<input type="text" placeholder="Entrez votre pseudo" id="pseudo-input" required autofocus><br>
@@ -30,15 +32,17 @@ window.onload = function() {
 	const sock = new WebSocket("ws://" + window.location.host);
 
 	const connectedPlayersCount = document.getElementById("connected-players");
-	const remainingPlayersCount = document.getElementById("remaining-players");
+	const minPlayersCount = document.getElementById("min-players");
 
 	const pseudoInput = document.getElementById("pseudo-input");
 	const sendPseudoButton = document.getElementById("send-pseudo-button");
 
-	const playerList = document.getElementById("player-list");
+	const playersHtmlList = document.getElementById("player-list");
+
+	let players;
 
 	sock.onopen = function() {
-		let state = STATE_STATS;
+		let state = STATE_GAME_INFO;
 
 		sock.send(JSON.stringify([CLIENT_TYPE_PLAYER]));
 
@@ -46,30 +50,52 @@ window.onload = function() {
 			let msg = JSON.parse(json.data);
 
 			switch(state) {
-				case STATE_STATS:
-					connectedPlayersCount.innerHTML = msg[0];
-					remainingPlayersCount.innerHTML = msg[1];
+				case STATE_GAME_INFO:
+					players = msg[1];
 
-					let playersPseudos = msg[2];
+					minPlayersCount.innerHTML = msg[0];
+					connectedPlayersCount.innerHTML = players.length;
 
-					playersPseudos.forEach(function(pseudo) {
-						let playerLine = document.createElement("div");
-						playerLine.classList.add("player-list-player");
-						playerLine.innerHTML = pseudo;
+					for(player of players) {
+						player.htmlLine = document.createElement("div");
+						player.htmlLine.classList.add("player-list-player");
+						player.htmlLine.innerHTML = player.pseudo;
 
-						playerList.appendChild(playerLine);
-					})
+						playersHtmlList.appendChild(player.htmlLine);
+					}
 
 					state = STATE_WAITING_ROOM;
 					break;
 
 				case STATE_WAITING_ROOM:
 					if(msg[0] == ADD_PLAYER) {
-						let playerLine = document.createElement("div");
-						playerLine.classList.add("player-list-player");
-						playerLine.innerHTML = msg[1];
+						let player = {id: msg[1], pseudo: msg[2]};
+						players.push(player);
 
-						playerList.appendChild(playerLine);
+						player.htmlLine = document.createElement("div");
+						player.htmlLine.classList.add("player-list-player");
+						player.htmlLine.innerHTML = player.pseudo;
+
+						playersHtmlList.appendChild(player.htmlLine);
+
+						connectedPlayersCount.innerHTML = players.length;
+					} else if(msg[0] == SET_PSEUDO) {
+						for(player of players) { // on récupère le joueur concerné grâce à son id
+							if(player.id == msg[1]) {
+								player.pseudo = msg[2]; // on change le pseudo du joueur
+								player.htmlLine.innerHTML = player.pseudo; // on met à jour la liste des joueurs HTML
+								break;
+							}
+						}
+					} else if(msg[0] == DEL_PLAYER) {
+						for(player of players) {
+							if(player.id == msg[1]) {
+								player.htmlLine.remove();
+								players.splice(players.indexOf(player), 1);
+
+								connectedPlayersCount.innerHTML = players.length;
+							}
+						}
 					}
 
 					break;
@@ -78,6 +104,6 @@ window.onload = function() {
 
 		sendPseudoButton.onclick = function() {
 			sock.send(JSON.stringify([pseudoInput.value]));
-		}
+		};
 	};
 }
--- code/server/websocket.js
+++ code/server/websocket.js
@@ -4,11 +4,13 @@ const CLIENT_TYPE_PLAYER = 0;
 const CLIENT_TYPE_SCREEN = 1;
 
 const ADD_PLAYER = 0;
-const DEL_PLAYER = 1;
+const SET_PSEUDO = 1;
+const DEL_PLAYER = 2;
 
 const STATE_NONE = 0;
-const STATE_AUTH = 1;
-const STATE_PSEUDO = 2;
+const STATE_WAITING_ROOM = 1;
+const STATE_AUTH = 2;
+const STATE_PSEUDO = 3;
 
 const SCREEN_SECRET_KEY = "piccascreendilly447";
 
@@ -18,42 +20,85 @@ module.exports = function(httpServer) {
 	const wss = new ws.Server({server: httpServer});
 
 	let screenSock;
-	const players = [];
+	const playersSocks = [];
+
+	let state = STATE_WAITING_ROOM;
+
+	let nextPlayerId = 0;
 
 	wss.on("connection", function(sock) {
-		sock.state = STATE_AUTH;
+		if(state == STATE_WAITING_ROOM) {
+			sock.state = STATE_AUTH;
 
-		let playersPseudos = [];
+			sock.on("message", function(json) {
+				let msg = JSON.parse(json);
 
-		players.forEach(function(player) {
-			playersPseudos.push(player.pseudo);
-		})
+				switch(state) {
+					case STATE_WAITING_ROOM:
+						switch(sock.state) {
+							case STATE_AUTH:
+								if(msg[0] == CLIENT_TYPE_PLAYER) {
+									sock.player = {};
+									sock.player.id = nextPlayerId;
+									nextPlayerId++;
 
-		sock.send(JSON.stringify([players.length, MIN_PLAYER, playersPseudos]));
+									sock.player.pseudo = "???";
 
-		sock.on("message", function(json) {
-			let msg = JSON.parse(json);
+									playersSocks.push(sock);
 
-			switch(sock.state) {
-				case STATE_AUTH:
-					if(msg[0] == CLIENT_TYPE_PLAYER) {
-						players.push(sock);
-						sock.state = STATE_PSEUDO;
-					} else if(msg[0] == CLIENT_TYPE_SCREEN && msg[1] == SCREEN_SECRET_KEY && !screenSock) {
-						screenSock = sock;
+									let players = [];
+
+									for(playerSock of playersSocks) {
+										players.push(playerSock.player);
+										playerSock.send(JSON.stringify([ADD_PLAYER, sock.player.id, sock.player.pseudo]));
+									}
+
+									sock.send(JSON.stringify([MIN_PLAYER, players]));
+
+									sock.state = STATE_PSEUDO;
+								} else if(msg[1] == CLIENT_TYPE_SCREEN) {
+									screenSock = sock;
+									sock.state = STATE_NONE
+								}
+
+								break;
+
+							case STATE_PSEUDO:
+								let isPseudoFree = true;
+
+								for(playerSock of playersSocks) {
+									if(msg[0] == playerSock.player.pseudo) {
+										isPseudoFree = false;
+										break;
+									}
+								}
+
+								if(isPseudoFree) {
+									sock.player.pseudo = msg[0];
+
+									for(playerSock of playersSocks) {
+										playerSock.send(JSON.stringify([SET_PSEUDO, sock.player.id, sock.player.pseudo]));
+									}
+
+									sock.state = STATE_NONE;
+								}
+
+								break;
+						}
+
+						break;
+				}
+			});
+
+			sock.on("close", function() {
+				if(state == STATE_WAITING_ROOM && sock.player) {
+					playersSocks.splice(playersSocks.indexOf(sock), 1);
+
+					for(playerSock of playersSocks) {
+						playerSock.send(JSON.stringify([DEL_PLAYER, sock.player.id]));
 					}
-
-					break;
-
-				case STATE_PSEUDO:
-					sock.pseudo = msg[0];
-
-					players.forEach(function(player) {
-						player.send(JSON.stringify([ADD_PLAYER, sock.pseudo]));
-					});
-
-					break;
-			}
-		});
+				}
+			});
+		}
 	});
 }
