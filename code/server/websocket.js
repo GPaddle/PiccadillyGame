50bf3e568c3b51d43848841c4b51c00df865f6c3
Keller Guillaume <keller.guillaume68@gmail.com>
2020-01-03 00:41:38 +0100

    passage à l'état GAME lorque le nombre de joueur est bon

--- code/server/websocket.js
+++ code/server/websocket.js
@@ -8,7 +8,7 @@ const SET_PSEUDO = 1;
 const DEL_PLAYER = 2;
 const PSEUDO_ALREADY_USED = 3;
 
-const State = {
+const states = {
     NONE: 0,
     WAITING_ROOM: 1,
     AUTH: 2,
@@ -18,7 +18,7 @@ const State = {
 
 const SCREEN_SECRET_KEY = "7116dd23254dc1a8";
 
-const MIN_PLAYER = 2;
+const MIN_PLAYER = 1;
 
 module.exports = function(httpServer) {
     const wss = new ws.Server({ server: httpServer });
@@ -26,21 +26,22 @@ module.exports = function(httpServer) {
     let screenSock;
     const playersSocks = [];
 
-    let state = State.WAITING_ROOM;
+    let state = states.WAITING_ROOM;
 
     let nextPlayerId = 0;
 
     wss.on("connection", function(sock) {
-        if (state == State.WAITING_ROOM) {
-            sock.state = State.AUTH;
+        if (state == states.WAITING_ROOM) {
+            sock.state = states.AUTH;
 
             sock.on("message", function(json) {
                 let msg = JSON.parse(json);
-                console.log(msg);
+                console.log("Websocket : " + msg);
+
                 switch (state) {
-                    case State.WAITING_ROOM:
+                    case states.WAITING_ROOM:
                         switch (sock.state) {
-                            case State.AUTH:
+                            case states.AUTH:
                                 if (msg[0] == CLIENT_TYPE_PLAYER) {
                                     sock.player = {};
                                     sock.player.id = nextPlayerId;
@@ -61,17 +62,18 @@ module.exports = function(httpServer) {
 
                                     sock.send(JSON.stringify([MIN_PLAYER, players, sock.player.id]));
 
-                                    sock.state = State.PSEUDO;
+                                    sock.state = states.PSEUDO;
+
                                 } else if (msg[0] == CLIENT_TYPE_SCREEN && msg[1] == SCREEN_SECRET_KEY) {
                                     screenSock = sock;
-                                    sock.state = State.NONE;
+                                    sock.state = states.NONE;
 
                                     sock.send(JSON.stringify([MIN_PLAYER, playersSocks.length]));
                                 }
 
                                 break;
 
-                            case State.PSEUDO:
+                            case states.PSEUDO:
                                 let isPseudoFree = true;
 
                                 for (playerSock of playersSocks) {
@@ -85,29 +87,48 @@ module.exports = function(httpServer) {
                                     sock.player.pseudo = msg[0];
 
                                     for (playerSock of playersSocks) {
+                                        /**
+                                         * JSON
+                                         * 
+                                         * pseudo
+                                         * idPlayer
+                                         * pseudoPlayer
+                                         */
                                         playerSock.send(JSON.stringify([SET_PSEUDO, sock.player.id, sock.player.pseudo]));
                                     }
 
-                                    sock.state = State.NONE;
+                                    sock.state = states.NONE;
+                                    if (playersSocks.length >= MIN_PLAYER) {
+                                        console.log(state + " -> " + states.GAME);
+                                        state = states.GAME;
+                                    }
+
                                 } else {
                                     sock.send(JSON.stringify([PSEUDO_ALREADY_USED]));
                                 }
 
                                 break;
+
+
                         }
 
-                        /*
-                        					case State.GAME:
-                        						switch (sock.state) {
-                        							case State.
-                        						}
-                        */
+
+
+                        break;
+                    case states.GAME:
+
+                        for (playerSocks of playersSocks) {
+                            playerSock.state = states.GAME;
+                        }
+
+                        console.log(msg);
+                        screenSock.send(JSON.stringify(msg));
                         break;
                 }
             });
 
             sock.on("close", function() {
-                if (state == State.WAITING_ROOM && sock.player) {
+                if (state == states.WAITING_ROOM && sock.player) {
                     playersSocks.splice(playersSocks.indexOf(sock), 1);
 
                     screenSock.send(JSON.stringify([DEL_PLAYER]));
