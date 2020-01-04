08b636d86ef940cc0a6af09246061c2334430b2d
daiSKeul <martin.jossic5@etu.univ-lorraine.fr>
2020-01-04 15:01:16 +0100

    Les variables de screen.js sont plus locales

--- code/client/screen/screen.js
+++ code/client/screen/screen.js
@@ -2,10 +2,10 @@
 
 const CLIENT_TYPE_SCREEN = 1;
 
-const ADD_PLAYER = 0;
-const DEL_PLAYER = 2;
-const START_GAME_COUNTDOWN = 4;
-const START_GAME = 5;
+const ADD_PLAYER = 0, 
+	DEL_PLAYER = 2, 
+	START_GAME_COUNTDOWN = 4, 
+	START_GAME = 5;
 
 const NEW_QUESTION = 0,
 	END_GAME = 3;
@@ -25,18 +25,6 @@ window.onload = function() {
 	<div id="players-info">Nombre de joueurs connectés : <span id="players-count">0</span></div>
 	<div id="min-players-info">Nombre de joueurs minimum nécessaires : <span id="min-players-count">...</span></div>
 	`;
-
-	let playersInfoHtml = document.getElementById("players-info");
-
-	let playersCountHtml = document.getElementById("players-count");
-	let minPlayersCountHtml = document.getElementById("min-players-count");
-
-	let questionNumberHtml;
-	let questionHtml;
-	let questionInfoHtml;
-
-	let answersHtml;
-	let answersTextHtml;
 
 	function displayGame() {
 		document.body.innerHTML = `
@@ -60,13 +48,6 @@ window.onload = function() {
 			<span class="answer-text">...</div>
 		</div>
 		`;
-
-		questionNumberHtml = document.getElementById("question-number");
-		questionHtml = document.getElementById("question");
-		questionInfoHtml = document.getElementById("question-info");
-
-		answersHtml = document.getElementsByClassName("answer");
-		answersTextHtml = document.getElementsByClassName("answer-text");
 	}
 
 	const sock = new WebSocket("ws://" + window.location.host);
@@ -75,8 +56,6 @@ window.onload = function() {
 		let state = STATE_GAME_INFO;
 
 		let playersCount = 0;
-		let minPlayersCount = 0;
-
 		let question;
 
 		let questionNumber = 0;
@@ -89,11 +68,13 @@ window.onload = function() {
 
 			switch (state) {
 				case STATE_GAME_INFO: {
-					minPlayersCount = msg[0];
-					minPlayersCountHtml.innerHTML = minPlayersCount;
+					let minPlayersCount = document.getElementById("min-players-count");
+					minPlayersCount.innerHTML = msg[0];
 
 					playersCount = msg[1];
-					playersCountHtml.innerHTML = playersCount;
+
+					let playersCountSpan = document.getElementById("players-count");
+					playersCountSpan.innerHTML = playersCount;
 
 					state = STATE_WAITING_ROOM;
 					break;
@@ -102,26 +83,32 @@ window.onload = function() {
 				case STATE_WAITING_ROOM: {
 					if(msg[0] == ADD_PLAYER) {
 						playersCount++;
-						playersCountHtml.innerHTML = playersCount;
+
+						let playersCountSpan = document.getElementById("players-count");
+						playersCountSpan.innerHTML = playersCount;
 					} else if(msg[0] == DEL_PLAYER) {
 						playersCount--;
-						playersCountHtml.innerHTML = playersCount;
+
+						let playersCountSpan = document.getElementById("players-count");
+						playersCountSpan.innerHTML = playersCount;
 					} else if(msg[0] == START_GAME_COUNTDOWN) {
-						let countdownInfoHtml = document.createElement("div");
-						countdownInfoHtml.id = "start-countdown-info";
-						countdownInfoHtml.innerHTML = "La partie commence dans ";
+						let countdownInfo = document.createElement("div");
+						countdownInfo.id = "start-countdown-info";
+						countdownInfo.innerHTML = "La partie commence dans ";
 
 						let time = msg[1];
 
-						let countdownHtml = document.createElement("span");
-						countdownHtml.innerHTML = time;
+						let countdownSpan = document.createElement("span");
+						countdownSpan.innerHTML = time;
 
-						countdownInfoHtml.appendChild(countdownHtml);
-						document.body.insertBefore(countdownInfoHtml, playersInfoHtml);
+						countdownInfo.appendChild(countdownSpan);
+
+						let playersInfo = document.getElementById("players-info");
+						document.body.insertBefore(countdownInfo, playersInfo);
 
 						let countdown = setInterval(function() {
 							time--;
-							countdownHtml.innerHTML = time;
+							countdownSpan.innerHTML = time;
 
 							if(time == 0) {
 								clearInterval(countdown);
@@ -139,31 +126,39 @@ window.onload = function() {
 					if(msg[0] == NEW_QUESTION) {
 						clearInterval(questionCountdown);
 
-						for(let answerHtml of answersHtml) {
-							answerHtml.style.backgroundColor = "";
+						let answers = document.getElementsByClassName("answer");
+
+						for(let answer of answers) {
+							answer.style.backgroundColor = "";
 						}
 
 						question = msg[1];
 
 						questionNumber++;
-						questionNumberHtml.innerHTML = "Question " + questionNumber;
 
-						questionHtml.innerHTML = question[0];
+						let questionNumberDiv = document.getElementById("question-number");
+						questionNumberDiv.innerHTML = "Question " + questionNumber;
+
+						let questionDiv = document.getElementById("question");
+						questionDiv.innerHTML = question[0];
+
+						let answersText = document.getElementsByClassName("answer-text");
 
 						for(let i = 0; i < 4; i++) {
-							answersTextHtml[i].innerHTML = question[1][i];
+							answersText[i].innerHTML = question[1][i];
 						}
 
 						let time = question[2];
 
-						questionInfoHtml.innerHTML = "Temps restant : <span id=\"question-countdown\">...</span>";
+						let questionInfo = document.getElementById("question-info");
+						questionInfo.innerHTML = "Temps restant : <span id=\"question-countdown\">...</span>";
 
-						let questionCountdownHtml = document.getElementById("question-countdown");
-						questionCountdownHtml.innerHTML = time;
+						let questionCountdownSpan = document.getElementById("question-countdown");
+						questionCountdownSpan.innerHTML = time;
 
 						questionCountdown = setInterval(function() {
 							time--;
-							questionCountdownHtml.innerHTML = time;
+							questionCountdownSpan.innerHTML = time;
 
 							if(time == 0) {
 								clearInterval(questionCountdown);
@@ -177,11 +172,11 @@ window.onload = function() {
 						<div id="results"></div>
 						`;
 
-						let resultsHtml = document.getElementById("results");
+						let resultsDiv = document.getElementById("results");
 						let scores = msg[1];
 
 						for(let i = 0; i < scores.length; i++) {
-							resultsHtml.innerHTML += "<div class=\"resultContener\"><span class=\"playerName\">" + scores[i][0] + "</span><span class=\"playerScore\">" + scores[i][1] + "</span></div>";
+							resultsDiv.innerHTML += "<div class=\"resultContener\"><span class=\"playerName\">" + scores[i][0] + "</span><span class=\"playerScore\">" + scores[i][1] + "</span></div>";
 						}
 					}
 
@@ -190,13 +185,16 @@ window.onload = function() {
 				}
 
 				case STATE_ANSWER: {
-					questionInfoHtml.innerHTML = "La bonne réponse était \"" + question[1][msg[0]] + "\"";
+					let questionInfo = document.getElementById("question-info");
+					questionInfo.innerHTML = "La bonne réponse était \"" + question[1][msg[0]] + "\"";
 
-					for(let answerHtml of answersHtml) {
-						answerHtml.style.backgroundColor = "#bb0b0b";
+					let answers = document.getElementsByClassName("answer");
+
+					for(let answer of answers) {
+						answer.style.backgroundColor = "#bb0b0b";
 					}
 
-					answersHtml[msg[0]].style.backgroundColor = "#22780f";
+					answers[msg[0]].style.backgroundColor = "#22780f";
 
 					state = STATE_WAIT_QUESTION;
 					break;
