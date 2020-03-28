b3884409f1cae99e14dc64fac8e8f1a77078fa85
daiSKeul <martin.jossic5@etu.univ-lorraine.fr>
2020-03-28 14:44:58 +0100

    Factorisation du code de main.js

--- code/server/main.js
+++ code/server/main.js
@@ -10,139 +10,32 @@ const httpServer = http.createServer();
 httpServer.listen(conf.port);
 
 httpServer.on("request", function(req, res) {
+	function send(url, type) {
+		res.setHeader("Content-Type", type);
+		let file = fs.createReadStream(url);
+		file.pipe(res);
+	}
+
 	switch (req.url) {
-		case "/": {
-			res.end("Piccadilly Game");
-			break;
-		}
-
-		case "/play": {
-			res.setHeader("Content-Type", "text/html");
-			let file = fs.createReadStream("client/play/play.html");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen": {
-			res.setHeader("Content-Type", "text/html");
-			let file = fs.createReadStream("client/screen/screen.html");
-			file.pipe(res);
-			break;
-		}
-
-		case "/play.css": {
-			res.setHeader("Content-Type", "text/css");
-			let file = fs.createReadStream("client/play/play.css");
-			file.pipe(res);
-			break;
-		}
-
-		case "/play.js": {
-			res.setHeader("Content-Type", "application/javascript");
-			let file = fs.createReadStream("client/play/play.js");
-			file.pipe(res);
-			break;
-		}
-
-		case "/play/game.js": {
-			res.setHeader("Content-Type", "application/javascript");
-			let file = fs.createReadStream("client/play/" + conf.game + ".js");
-			file.pipe(res);
-			break;
-		}
-
-		case "/play/starship.png": {
-			res.setHeader("Content-Type", "image/png");
-			let file = fs.createReadStream("client/play/starship.png");
-			file.pipe(res);
-			break;
-		}
-
-		case "/qrious.js": {
-			res.setHeader("Content-Type", "application/javascript");
-			let file = fs.createReadStream("node_modules/qrious/dist/qrious.js");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen.js": {
-			res.setHeader("Content-Type", "application/javascript");
-			let file = fs.createReadStream("client/screen/screen.js");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/game.js": {
-			res.setHeader("Content-Type", "application/javascript");
-			let file = fs.createReadStream("client/screen/" + conf.game + ".js");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/background.svg": {
-			res.setHeader("Content-Type", "image/svg+xml");
-			let file = fs.createReadStream("client/screen/assets/background/background.svg");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/moon.svg": {
-			res.setHeader("Content-Type", "image/svg+xml");
-			let file = fs.createReadStream("client/screen/assets/background/moon.svg");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/stars1.svg": {
-			res.setHeader("Content-Type", "image/svg+xml");
-			let file = fs.createReadStream("client/screen/assets/background/stars1.svg");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/stars2.svg": {
-			res.setHeader("Content-Type", "image/svg+xml");
-			let file = fs.createReadStream("client/screen/assets/background/stars2.svg");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/laser-top.png": {
-			res.setHeader("Content-Type", "image/png");
-			let file = fs.createReadStream("client/screen/assets/laser-top.png");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen/starship.png": {
-			res.setHeader("Content-Type", "image/png");
-			let file = fs.createReadStream("client/screen/assets/starship.png");
-			file.pipe(res);
-			break;
-		}
-
-		case "/screen.css": {
-			res.setHeader("Content-Type", "text/css");
-			let file = fs.createReadStream("client/screen/screen.css");
-			file.pipe(res);
-			break;
-		}
-
-		case "/fonts/lato": {
-			res.setHeader("Content-Type", "font/ttf");
-			res.setHeader("Content-Length", 75136);
-			let file = fs.createReadStream("client/fonts/lato/lato.ttf");
-			file.pipe(res);
-			break;
-		}
-
-		case "/fonts/lato-bold": {
-			res.setHeader("Content-Type", "font/ttf");
-			res.setHeader("Content-Length", 73316);
-			let file = fs.createReadStream("client/fonts/lato/lato-bold.ttf");
-			file.pipe(res);
-			break;
-		}
+		case "/": res.end("Piccadilly Game"); break;
+		case "/play": send("client/play/play.html", "text/html"); break;
+		case "/screen": send("client/screen/screen.html", "text/html"); break;
+		case "/play.css": send("client/play/play.css", "text/css"); break;
+		case "/play.js": send("client/play/play.js", "application/javascript"); break;
+		case "/play/game.js": send("client/play/" + conf.game + ".js", "application/javascript"); break;
+		case "/play/starship.png": send("client/play/starship.png", "image/png"); break;
+		case "/qrious.js": send("node_modules/qrious/dist/qrious.js", "application/javascript"); break;
+		case "/screen.js": send("client/screen/screen.js", "application/javascript"); break;
+		case "/screen/game.js": send("client/screen/" + conf.game + ".js", "application/javascript"); break;
+		case "/screen/background.svg": send("client/screen/assets/background/background.svg", "image/svg+xml"); break;
+		case "/screen/moon.svg": send("client/screen/assets/background/moon.svg", "image/svg+xml"); break;
+		case "/screen/stars1.svg": send("client/screen/assets/background/stars1.svg", "image/svg+xml"); break;
+		case "/screen/stars2.svg": send("client/screen/assets/background/stars2.svg", "image/svg+xml"); break;
+		case "/screen/laser-top.png": send("client/screen/assets/laser-top.png", "image/png"); break;
+		case "/screen/starship.png": send("client/screen/assets/starship.png", "image/png"); break;
+		case "/screen.css": send("client/screen/screen.css", "text/css"); break;
+		case "/fonts/lato": send("client/fonts/lato/lato.ttf", "font/ttf"); break;
+		case "/fonts/lato-bold": send("client/fonts/lato/lato-bold.ttf", "font/ttf"); break;
 
 		default: {
 			res.statusCode = 404;
